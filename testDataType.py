a47c7a3d3986fdba0959a42fdf22a61857fc3453
Serey Neath <sereyneath@Sereys-MacBook-Pro.local>
2024-07-20 11:29:48 +0700

    number

--- testDataType.py
+++ testDataType.py
@@ -1,6 +1,22 @@
-str = "Hello World "
-print(str)
-print(str[0])
-print(str[2:5])
-print(str*10)
-print(str+" From BBU")
+#string
+# str_txt = "Hello World "
+# print(str)
+# print(str[0])
+# print(str[2:5])
+# print(str*10)
+# print(str+" From BBU")
+# print(type(str))
+#
+# #Number
+# a = 1_000_000
+# print(a)
+# print(type(a))
+# b = 12.22
+# print(b)
+# print(type(b))
+
+val1 = int("20")
+val2 = int("30")
+total = val1+val2
+print("Total = "+ str(total))
+print(len(str(total)))
